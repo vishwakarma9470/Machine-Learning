3c8cdec57178044a361554e0909f449a25d53c80
Manish Vishwakarma <manishvishawakarma2003@gmail.com>
2025-06-08 20:51:08 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="manish",
+    author_email="mv947027@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
